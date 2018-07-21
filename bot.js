0ad1d4cf20e81a6e4ee242744cab04932c4ae03b
wadda7-alshammari <41497837+wadda7-alshammari@users.noreply.github.com>
2018-07-21 04:19:44 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,6 +53,39 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
 
 
 
+ client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 1,
+        maxAge: 3600,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription("تم أرسال الرابط برسالة خاصة")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
+
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        
+    .setDescription("** مدة الرابط : ساعه | عدد استخدامات الرابط : 1 **")
+      message.author.sendEmbed(Embed11)
+    }
+}); 
+
+
+
+
+
+
+
+
+
 
 
 
