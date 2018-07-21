03d13e35ffad8a8e1596af5d41143d98a1091102
wadda7-alshammari <41497837+wadda7-alshammari@users.noreply.github.com>
2018-07-21 05:13:57 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -164,6 +164,30 @@ client.on('message', message => {
 
 
 
+client.on('voiceStateUpdate', (old, now) => {
+  const channel = client.channels.get('470095592228388865');
+  const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
+  const size = channel.name.match(/\[\s(\d+)\s\]/);
+  if (!size) return channel.setName(`Voice Online: ${currentSize}`);
+  if (currentSize !== size) channel.setName(`Voice Online: ${currentSize}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
