751f51aa97cbd579c0c47ce299723aaf5db70526
wadda7-alshammari <41497837+wadda7-alshammari@users.noreply.github.com>
2018-07-22 03:22:21 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -84,10 +84,10 @@ client.on('voiceStateUpdate', (old, now) => {
 
 
   client.on("message", message => {
-    var prefix = "~";
+    var prefix = "";
  
             var args = message.content.substring(prefix.length).split(" ");
-            if (message.content.startsWith(prefix + "clear")) {
+            if (message.content.startsWith(prefix + "مسح")) {
    if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **لا يوجد لديك صلاحية لمسح الشات**');
         var msg;
         msg = parseInt();
